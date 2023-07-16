3b42c1c268b738f990bf22981960e5eee2e6ae10
simon <simon@host.docker.internal>
2023-07-16 14:32:06 +0300

    first commit

---
+++ src/main/java/com/bot/service/BotServiceApplication.java
@@ -0,0 +1,16 @@
+package com.bot.service;
+
+import org.apache.log4j.Logger;
+
+import com.bot.service.bot.Bot;
+
+public class BotServiceApplication {
+	private static final Logger log = Logger.getLogger(BotServiceApplication.class);
+
+	public static void main(String[] args) {
+//		Bot myBot = new Bot();
+//		myBot.s
+//		myBot.botConnect();
+
+	}
+}
